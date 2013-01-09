b89251d7bd380a8ae9be10187bbda4ad3c8d3b77
Stephen Colebourne <stephen@opengamma.com>
2013-01-09 12:03:34 +0000

    Update license headers

--- src/main/java/org/fudgemsg/proto/FieldType.java
+++ src/main/java/org/fudgemsg/proto/FieldType.java
@@ -1,4 +1,5 @@
-/* Copyright 2009 by OpenGamma Inc and other contributors.
+/*
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.fudgemsg.proto;
 
 import org.fudgemsg.wire.types.FudgeWireType;
--- src/main/java/org/fudgemsg/proto/GlobalDefault.java
+++ src/main/java/org/fudgemsg/proto/GlobalDefault.java
@@ -1,4 +1,5 @@
-/* Copyright 2009 by OpenGamma Inc and other contributors.
+/*
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
-
+ */
 package org.fudgemsg.proto;
 
 public class GlobalDefault {
